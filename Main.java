fd488572bf90181df3354d329b3bd17b407c90f1
Muskan <69207355+Muskan10Mehta@users.noreply.github.com>
2022-02-01 22:29:47 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,16 +7,23 @@
  * Date: 31-01-2022
  */
 
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 public class Main
 {
     public static void main(String[] args)
     {
         // Introduction text   
-        System.out.println("Welcome to Wordle Java Command Line Version");
-        System.out.println("*******************************************");
-        System.out.println("How to play: \nWordle originally created by Josh Wardle, is a game where the player gets 6 chances to guess a 5-letter word, \nthe player can try guessing the word by trying different 5-letter words,\nwhen the player enters a guess, they will be given a hint about how close the word is to the game word. \n If the game word is: h e l l o \n and Player guesses h o l e s \nthe game will give the following hint \n h at 1 is at the right place\n l at 3 is at the right place\n e is not at the right place, but present in the game word\n o is not at the right place but present in the game word. \nJust like that keep guessing and remember you only get 6 chances.\n");
+        File introduction = new File("rules.txt"); //opens file rules.txt
+        Scanner reader = new Scanner(introduction);
+        
+        while(reader.hasNextLine())
+        {
+            String line = reader.nextLine();
+            System.out.println(line);
+        }
+        reader.close();
         
         Scanner sc = new Scanner(System.in);
 
